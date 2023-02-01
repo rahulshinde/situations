c6286281e279893268ac2772fce9683d7fde7a16
Rahul S. Shinde <rsubhashshinde@gmail.com>
2023-02-01 11:04:20 -0500

    new modules for calculating tethers

--- js/slime.js
+++ js/slime.js
@@ -7,7 +7,10 @@ import { LetterI } from './letters/i.js'
 import { LetterM } from './letters/m.js'
 import { LetterE } from './letters/e.js'
 
-const splineWidth = 2;
+import * as sceneBuilder from './utils/sceneBuilder.js'
+import * as tetherCalculator from './utils/tetherCalculator.js'
+
+const splineWidth = 2.5;
 const material = new THREE.MeshPhongMaterial( 
 	{ 
 		color: 0xf7d011 
@@ -25,8 +28,8 @@ const i = new LetterI(splineWidth, material)
 const m = new LetterM(splineWidth, material)
 const e = new LetterE(splineWidth, material)
 
-const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 4000 );
-camera.position.z = 125;
+const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.z = 200;
 
 const renderer = new THREE.WebGLRenderer({
 	antialias: true,
@@ -43,19 +46,20 @@ cameraControls.minDistance = 30;
 cameraControls.update();
 
 const scene = new THREE.Scene();
+const group = new THREE.Group();
 
-scene.add(s.path);
-scene.add(l.path);
-scene.add(i.path);
-scene.add(m.path);
-scene.add(e.path);
+group.add(s.path);
+group.add(l.path);
+group.add(i.path);
+group.add(m.path);
+group.add(e.path);
 
 
-s.path.position.x = -10;
-s.path.position.y = 80;
+s.path.position.x = -80;
+s.path.position.y = 40;
 s.path.position.z = -30;
 
-l.path.position.x = -60;
+l.path.position.x = -20;
 l.path.position.y = -0;
 l.path.position.z = 20;
 
@@ -63,112 +67,18 @@ i.path.position.x = 10;
 i.path.position.y = 40;
 i.path.position.z = 30;
 
-m.path.position.x = 0;
+m.path.position.x = 40;
 m.path.position.y = 0;
 m.path.position.z = -20;
 
-e.path.position.x = 10;
+e.path.position.x = 80;
 e.path.position.y = -40;
 e.path.position.z = -45;
 
-// s to l
+sceneBuilder.bulidTethers([s, l, i, m, e], group, splineWidth);
 
-let connector1a = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( s.exit1[0] + s.path.position.x, s.exit1[1] + s.path.position.y, s.exit1[2] + s.path.position.z ),		new THREE.Vector3( l.enter1[0] + l.path.position.x, l.enter1[1] + l.path.position.y, l.enter1[2] + l.path.position.z )
-	]
-)
+scene.add(group);
 
-let connector1b = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( s.exit2[0] + s.path.position.x, s.exit2[1] + s.path.position.y, s.exit2[2] + s.path.position.z ),
-		new THREE.Vector3( l.enter2[0] + l.path.position.x, l.enter2[1] + l.path.position.y, l.enter2[2] + l.path.position.z )
-	]
-)
-
-let geometryConnector1a = new THREE.TubeGeometry( connector1a, 60, splineWidth, 15, false );
-let geometryConnector1b = new THREE.TubeGeometry( connector1b, 60, splineWidth, 15, false );
-
-let meshConnector1a = new THREE.Mesh( geometryConnector1a, material2 );
-let meshConnector1b = new THREE.Mesh( geometryConnector1b, material2 );
-
-scene.add(meshConnector1a);
-scene.add(meshConnector1b);
-
-// l to i
-
-let connector2a = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( l.exit1[0] + l.path.position.x, l.exit1[1] + l.path.position.y, l.exit1[2] + l.path.position.z ),
-		new THREE.Vector3( i.enter1[0] + i.path.position.x, i.enter1[1] + i.path.position.y, i.enter1[2] + i.path.position.z )
-	]
-)
-
-let connector2b = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( l.exit2[0] + l.path.position.x, l.exit2[1] + l.path.position.y, l.exit2[2] + l.path.position.z ),
-		new THREE.Vector3( i.enter2[0] + i.path.position.x, i.enter2[1] + i.path.position.y, i.enter2[2] + i.path.position.z )
-	]
-)
-
-let geometryConnector2a = new THREE.TubeGeometry( connector2a, 60, splineWidth, 15, false );
-let geometryConnector2b = new THREE.TubeGeometry( connector2b, 60, splineWidth, 15, false );
-
-let meshConnector2a = new THREE.Mesh( geometryConnector2a, material2 );
-let meshConnector2b = new THREE.Mesh( geometryConnector2b, material2 );
-
-scene.add(meshConnector2a);
-scene.add(meshConnector2b);
-
-// i to m
-
-let connector3a = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( i.exit1[0] + i.path.position.x, i.exit1[1] + i.path.position.y, i.exit1[2] + i.path.position.z ),
-		new THREE.Vector3( m.enter1[0] + m.path.position.x, m.enter1[1] + m.path.position.y, m.enter1[2] + m.path.position.z )
-	]
-)
-
-let connector3b = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( i.exit2[0] + i.path.position.x, i.exit2[1] + i.path.position.y, i.exit2[2] + i.path.position.z ),
-		new THREE.Vector3( m.enter2[0] + m.path.position.x, m.enter2[1] + m.path.position.y, m.enter2[2] + m.path.position.z )
-	]
-)
-
-let geometryConnector3a = new THREE.TubeGeometry( connector3a, 60, splineWidth, 15, false );
-let geometryConnector3b = new THREE.TubeGeometry( connector3b, 60, splineWidth, 15, false );
-
-let meshConnector3a = new THREE.Mesh( geometryConnector3a, material2 );
-let meshConnector3b = new THREE.Mesh( geometryConnector3b, material2 );
-
-scene.add(meshConnector3a);
-scene.add(meshConnector3b);
-
-// i to m
-
-let connector4a = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( m.exit1[0] + m.path.position.x, m.exit1[1] + m.path.position.y, m.exit1[2] + m.path.position.z ),
-		new THREE.Vector3( e.enter1[0] + e.path.position.x, e.enter1[1] + e.path.position.y, e.enter1[2] + e.path.position.z )
-	]
-)
-
-let connector4b = new THREE.CatmullRomCurve3(
-	[
-		new THREE.Vector3( m.exit2[0] + m.path.position.x, m.exit2[1] + m.path.position.y, m.exit2[2] + m.path.position.z ),
-		new THREE.Vector3( e.enter2[0] + e.path.position.x, e.enter2[1] + e.path.position.y, e.enter2[2] + e.path.position.z )
-	]
-)
-
-let geometryConnector4a = new THREE.TubeGeometry( connector4a, 60, splineWidth, 15, false );
-let geometryConnector4b = new THREE.TubeGeometry( connector4b, 60, splineWidth, 15, false );
-
-let meshConnector4a = new THREE.Mesh( geometryConnector4a, material2 );
-let meshConnector4b = new THREE.Mesh( geometryConnector4b, material2 );
-
-scene.add(meshConnector4a);
-scene.add(meshConnector4b);
 
 const light = new THREE.AmbientLight( 0xffffff, 0.75 ); // soft white light
 scene.add( light );
@@ -182,9 +92,6 @@ scene.add( light1 );
 document.body.appendChild( renderer.domElement );
 
 function animation( time ) {
+	// group.rotation.y = time / 3000;
 	renderer.render( scene, camera );
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
 }
---
+++ js/utils/sceneBuilder.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+import * as tetherCalculator from './tetherCalculator.js'
+
+const tetherMaterial = new THREE.MeshPhongMaterial( 
+	{ 
+		color: 0xf7d011 
+	} 
+);
+
+export function bulidTethers(letters, scene, splineWidth){
+	for (var i = 0; i < letters.length - 1; i++) {
+		var exit = letters[i];
+		var enter = letters[i + 1];
+
+		var exit1 = tetherCalculator.calculatePosition(exit.exit1, exit.path.position);
+		var enter1 = tetherCalculator.calculatePosition(enter.enter1, enter.path.position);
+		var connectorDisplacementA1 = tetherCalculator.calculateDisplacement(exit1, enter1);
+		var connectorDisplacementA2 = tetherCalculator.calculateDisplacement(exit1, enter1);
+
+		addTetherToScene(exit1, enter1, connectorDisplacementA1, connectorDisplacementA2, splineWidth, scene);
+
+		var exit2 = tetherCalculator.calculatePosition(exit.exit2, exit.path.position);
+		var enter2 = tetherCalculator.calculatePosition(enter.enter2, enter.path.position);
+		var connectorDisplacementB1 = tetherCalculator.calculateDisplacement(exit2, enter2);
+		var connectorDisplacementB2 = tetherCalculator.calculateDisplacement(exit2, enter2);
+		
+		addTetherToScene(exit2, enter2, connectorDisplacementB1, connectorDisplacementB2, splineWidth, scene);
+
+	}
+}
+
+function addTetherToScene(exit, enter, connectorDisplacement1, connectorDisplacement2, splineWidth, scene){
+
+	let tether = new THREE.CatmullRomCurve3(
+		[
+			new THREE.Vector3( exit.x, exit.y, exit.z ),		
+			new THREE.Vector3( connectorDisplacement1.x, connectorDisplacement1.y, connectorDisplacement1.z ),		
+			new THREE.Vector3( connectorDisplacement2.x, connectorDisplacement2.y, connectorDisplacement2.z ),		
+			new THREE.Vector3( enter.x, enter.y, enter.z )
+		]
+	)
+	let geometry = new THREE.TubeGeometry( tether, 120, splineWidth, 15, false );
+	let mesh = new THREE.Mesh( geometry, tetherMaterial );
+	scene.add(mesh);
+}
